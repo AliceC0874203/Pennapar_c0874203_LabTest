e20af9ac45febe0772b39a2c9d110eef94402923
Alice <c0874203@mylambton.ca>
2022-11-04 19:08:20 -0400

    add stopwatch and lap timestamp.

--- Pennapar_c0874203_LabTest/ViewController.swift
+++ Pennapar_c0874203_LabTest/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timingLabel: UILabel!
+    @IBOutlet weak var startTimerButton: UIButton!
+    @IBOutlet weak var lapButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var timer = Timer()
+    var seconds = 0.0
+    var isStarted = false
+    var lapTimeStamp = Array<String>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func runTimer() {
+        startTimerButton.setTitle("Stop", for: .normal)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer() {
+        startTimerButton.setTitle("Start", for: .normal)
+        lapButton.setTitle("Restart", for: .normal)
+        timer.invalidate()
+    }
+    
+    func resetTimer() {
+        seconds = 0.0
+        timingLabel.text = timeToString(time: TimeInterval(seconds))
+        lapButton.setTitle("Lap", for: .normal)
+        lapTimeStamp.removeAll()
+    }
+    
+    @objc func updateTimer() {
+        seconds += 1.0
+        timingLabel.text = timeToString(time: TimeInterval(seconds))
+        print(lapTimeStamp)
+    }
+    
+    func timeToString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i.%02i", hours, minutes, seconds)
+    }
 
-
+    @IBAction func controlTimeAction(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Start" {
+            runTimer()
+        } else {
+            stopTimer()
+        }
+    }
+    
+    @IBAction func lapAndResetAction(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Lap" {
+            lapTimeStamp.append(timingLabel.text ?? "00:00:00")
+        } else {
+            resetTimer()
+        }
+    }
 }
 
